9162db7aaf2fafb29bd486521c23a0b33f863c09
Lewis Smith <36924006+lewsmith@users.noreply.github.com>
2025-02-12 12:45:36 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Workers AI Image Playground",
+  title: "CF Workers AI Image Gen Example",
   description: "Playground for generating images with Cloudflare Workers AI models",
 };
 
